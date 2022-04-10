4f597f09634ccc30302e89faec956c88f9bcce52
Eugen Kovalchuk <kovalchuk.eak@gmail.com>
2022-04-10 21:56:43 +0300

    finish lazy-load

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from 'components/Layout/Layout';
-import { HomePage } from 'pages/HomePage';
-import { MoviesPage } from 'pages/MoviesPage';
-import { MovieDetailsPage } from 'pages/MovieDetailsPage';
-import { MovieDetailsCast } from 'pages/MovieDetailsCast';
-import { MovieDetailsReviews } from 'pages/MovieDetailsReviews';
-import { NotFoundPage } from 'pages/NotFoundPage';
+import { createAsyncView } from 'services/HelpFunctions';
+// import { HomePage } from 'pages/HomePage';
+// import { MoviesPage } from 'pages/MoviesPage';
+// import { MovieDetailsPage } from 'pages/MovieDetailsPage';
+// import { MovieDetailsCast } from 'pages/MovieDetailsCast';
+// import { MovieDetailsReviews } from 'pages/MovieDetailsReviews';
+// import { NotFoundPage } from 'pages/NotFoundPage';
+
+const HomePage = createAsyncView('HomePage');
+const MoviesPage = createAsyncView('MoviesPage');
+const MovieDetailsPage = createAsyncView('MovieDetailsPage');
+const MovieDetailsCast = createAsyncView('MovieDetailsCast');
+const MovieDetailsReviews = createAsyncView('MovieDetailsReviews');
+const NotFoundPage = createAsyncView('NotFoundPage');
 
 export const App = () => {
   return (
